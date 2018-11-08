91abfa39a70601712fb06a4bfe979f38a59d5fcb
Kobayashi <necrophantasia_koba@icloud.com>
2018-11-08 13:07:02 +0900

    Meta追加

---
+++ OlympicGames/Assets/Script/GamePlayerUIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayerUIController : MonoBehaviour {
+
+
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void Initialize()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        
+    }
+}
