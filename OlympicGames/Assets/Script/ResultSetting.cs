3eff07605daa508bd565bf0cd9394d076b7364b1
Fukunaga3617 <l7momip1z6junin3y@outlook.jp>
2018-11-09 10:14:30 +0900

    リザルト作成

--- OlympicGames/Assets/Script/ResultSetting.cs
+++ OlympicGames/Assets/Script/ResultSetting.cs
@@ -1,25 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ResultSetting : MonoBehaviour
 {
-				private GameObject rank_prafab = new GameObject();//順位のプレハブ
+				public GameObject rank_prafab;//順位のプレハブ
 
 				public List<Vector3> rank_ui_pos = new List<Vector3>();
 
+				public Sprite[] tex_rank = new Sprite[4];
+
+				public float time_min = 0;//最低描画時間
+				[SerializeField]
+				private float time = 0;
+				[SerializeField]
+				private bool is_scene_change = false;
+
 				void Start()
 				{
-								rank_prafab = (GameObject)Resources.Load("ResultRank");
-
 								List<int> result_data = GameResultManager.GetPlayerRank();
 								for (int i = 0;i < result_data.Count; i++)
 								{
 												GameObject obj = Instantiate(rank_prafab, Vector3.zero, Quaternion.identity);
-												obj.transform.position = rank_ui_pos[i];
+
+												obj.transform.parent = transform.parent;
+
+												obj.GetComponent<RectTransform>().localPosition = rank_ui_pos[i];
+
+												obj.transform.Find("Texter").GetComponent<Text>().text =
+																"Player" + i;
+
+												Image img = obj.transform.Find("Rank").GetComponent<Image>();
+												img.sprite = tex_rank[i];
 								}
 				}
-				
 
+				private void Update()
+				{
+								if(is_scene_change)
+								{
+												//シーンの変更を受け付けます
+												if (GamepadInput.GamePad.GetButton(GamepadInput.GamePad.Button.A, 0))
+												{
+																SceneManager.LoadScene("Menu");
+												}
+								}
 
+								time += Time.deltaTime;
+
+								if(time_min <= time)
+								{
+												is_scene_change = true;
+								}
+				}
 }
