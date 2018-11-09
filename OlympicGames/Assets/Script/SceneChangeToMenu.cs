b5f98392e0b912fdd5140963e4b2fa2ecf2a30ce
Fukunaga3617 <l7momip1z6junin3y@outlook.jp>
2018-11-09 11:38:38 +0900

    aaaa

--- OlympicGames/Assets/Script/SceneChangeToMenu.cs
+++ OlympicGames/Assets/Script/SceneChangeToMenu.cs
@@ -5,20 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeToMenu : MonoBehaviour
 {
+				[SerializeField, Tooltip("GameUpdatePreUpdateで要する時間を秒単位で指定")]
+				private float maxFadeUpdateTime = 1.0f; //一秒
+				[SerializeField]
+				private Fade fade = null;
+
+				bool is_fade_play = false;
+				System.Action fade_end;
+
 				// Use this for initialization
 				void Start()
 				{
 								ModeSetting.initialized();
+								fade_end = FadeOutChack;
+				}
+
+				void FadeOutChack()
+				{
+								if (GamepadInput.GamePad.GetButton(GamepadInput.GamePad.Button.A, (GamepadInput.GamePad.Index)0))
+								{
+												fade_end = FadeOut;
+								}
+				}
+
+				void FadeOut()
+				{
+								if(is_fade_play)
+								{
+												return;
+								}
+								is_fade_play = true;
+								fade.FadeOut(maxFadeUpdateTime, () =>
+								{
+												fade_end = FadeOutEnd;
+								});
+				}
+
+				void FadeOutEnd()
+				{
+								SceneManager.LoadScene("Menu");
 				}
 
 				// Update is called once per frame
 				void Update()
 				{
-								ModeSetting.ConnectedUpdate();
-
-								if (GamepadInput.GamePad.GetButton(GamepadInput.GamePad.Button.A, (GamepadInput.GamePad.Index)0))
-								{
-												SceneManager.LoadScene("Menu");
-								}
+								fade_end();
 				}
 }
